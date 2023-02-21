ff97adf5f80b2528a5c20e97687556e9d129e383
mconiaris <mconiaris@gmail.com>
2023-02-21 14:20:53 -0500

    More modifications to Website header.

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="App-header">
-            <div className="site-title">
+            <div className="site-title-left">
                 <img className="Header-image" 
                     alt="Code" 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
