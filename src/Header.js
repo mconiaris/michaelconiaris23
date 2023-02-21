6b5ff25fff0cfbc225eb5627d5c7dc890bfcf4f7
mconiaris <mconiaris@gmail.com>
2023-02-21 14:36:22 -0500

    Resized and moved images.

--- src/Header.js
+++ src/Header.js
@@ -4,17 +4,17 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="App-header">
-            <div className="site-title-left">
+            <div className="site-title-text">Michael Coniaris</div>
+            <div className="site-title">
                 <img 
-                    className="Header-image-left" 
+                    className="Header-image" 
                     alt="Code" 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
-                <div className="site-title-text">Michael Coniaris</div>
             </div>
-            <div className="site-title-right">
+            <div className="site-title">
                 <img 
-                    className="Header-image-left" 
+                    className="Header-image" 
                     alt="Link to About Page." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/Cat_and_Me_Small.jpeg">
                 </img>
