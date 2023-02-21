68c03b52a73b81822d5b154bdefe4312eaf54f9b
mconiaris <mconiaris@gmail.com>
2023-02-21 14:01:38 -0500

    Retructure and style website header.

--- src/Header.js
+++ src/Header.js
@@ -4,8 +4,13 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="App-header">
-            <h1>Michael Coniaris</h1>
-            <img alt="Code" src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg"></img>
+            <div className="site-title">
+                <img className="Header-image" 
+                    alt="Code" 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                </img>
+                <div className="site-title-text">Michael Coniaris</div>
+            </div>
         </header>
     )
 }
