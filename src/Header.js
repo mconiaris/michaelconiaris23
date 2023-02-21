aec87071e92d3256ceedf84074f0b6c72e42b361
mconiaris <mconiaris@gmail.com>
2023-02-21 14:25:56 -0500

    Header structure and style changes.

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,19 @@ export default function Header() {
     return (
         <header className="App-header">
             <div className="site-title-left">
-                <img className="Header-image" 
+                <img 
+                    className="Header-image-left" 
                     alt="Code" 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
                 <div className="site-title-text">Michael Coniaris</div>
+            </div>
+            <div className="site-title-right">
+                <img 
+                    className="Header-image-left" 
+                    alt="Link to About Page." 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/Cat_and_Me_Small.jpeg">
+                </img>
             </div>
         </header>
     )
