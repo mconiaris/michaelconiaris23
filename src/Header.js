b5bb71595023ff1976bb174cc6f1cdc2f08a80ff
mconiaris <mconiaris@gmail.com>
2023-03-01 14:06:02 -0500

    Modified CSS for header image text. Added border with slight green color.

--- src/Header.js
+++ src/Header.js
@@ -5,12 +5,12 @@ export default function Header() {
     return (
         <header className="App-header">
             <div className="Header-image">
-                <div className="site-title-text">Michael Coniaris</div>
                 <img 
                     alt="Michael Conairis's Website." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
             </div>
+            <div className="site-title-text">Michael Coniaris</div>
         </header>
     )
 }
