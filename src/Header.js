18ed2a5fb60fd8dfd534364f1c168591267721e3
mconiaris <mconiaris@gmail.com>
2023-03-01 13:20:17 -0500

    Removed photo from header section.

--- src/Header.js
+++ src/Header.js
@@ -11,13 +11,6 @@ export default function Header() {
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
             </div>
-            <div className="site-title">
-                <img 
-                    className="Header-image" 
-                    alt="Link to About Page." 
-                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/Cat_and_Me_Small.jpeg">
-                </img>
-            </div>
         </header>
     )
 }
