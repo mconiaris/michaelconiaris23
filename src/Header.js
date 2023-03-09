5872ab610d263e2a5224a5b122a9563353513ac4
mconiaris <mconiaris@gmail.com>
2023-03-09 11:14:11 -0500

    Wrapped site links into a container div.

--- src/Header.js
+++ src/Header.js
@@ -10,8 +10,10 @@ export default function Header() {
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
                 <div className="site-title-text">Michael Coniaris</div>
-                <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
-                <div className="site-link"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
+                <div className="site-link-container">
+                    <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
+                    <div className="site-link"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
+                </div>
             </div>
         </header>
     )
