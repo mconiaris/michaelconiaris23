d71c83cc6dc91cb20fabc451e6e40f1cd56a69e7
mconiaris <mconiaris@gmail.com>
2023-03-14 11:29:04 -0400

    Revised samples section so that it's centered. Added template text and images.

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,22 @@ import './Main.css';
 export default function Main() {
     return (
         <div className="samples-container">
-            <div className="samples-title">Samples</div>
+            <div className="samples-title">
+                <h1>Samples</h1>
+            </div>
+            
+            <div className="sample">
+                <h2>Example 1</h2>
+                <img 
+                    alt="Michael Conairis's Website." 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                </img>
+                <h2>Example 2</h2>
+                <img 
+                    alt="Michael Conairis's Website." 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                </img>
+            </div>
         </div>
     )
     
