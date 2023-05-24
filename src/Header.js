17088f7833f3340b71672c1a2ba79a677c7fe8ee
mconiaris <mconiaris@gmail.com>
2023-05-24 14:38:18 -0400

    Revised header title. Made it simpler with graphics on each size of the site name>

--- src/Header.js
+++ src/Header.js
@@ -9,12 +9,18 @@ export default function Header() {
                     alt="Michael Conairis's Website." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
-                <div className="site-title-text">Michael Coniaris</div>
-                <div className="site-link-container">
-                    <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
-                    <div className="site-link-divider"> | </div>
-                    <div className="site-link"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
-                </div>
+            </div>
+            <div className="site-title-text">Michael Coniaris</div>
+            <div className="Header-image">
+                <img 
+                    alt="Michael Conairis's Website." 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                </img>
+            </div>
+            <div className="site-link-container">
+                <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
+                <div className="site-link-divider"> | </div>
+                <div className="site-link"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
             </div>
         </header>
     )
