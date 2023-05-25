438f0008727c9aaa061a2b3618206de1bff89be8
mconiaris <mconiaris@gmail.com>
2023-05-25 13:34:39 -0400

    Cleared header CSS to reset. Tried to use correct naming conventions for React.

--- src/Header.js
+++ src/Header.js
@@ -3,21 +3,21 @@ import './Header.css';
 
 export default function Header() {
     return (
-        <header className="App-header top full">
-            <div className="Header-image">
+        <header className="AppHeader">
+            <div className="HeaderImage Top Full">
                 <img 
                     alt="Michael Conairis's Website." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
             </div>
-            <div className="site-title-text">Michael Coniaris</div>
-            <div className="Header-image">
+            <div className="SiteTitleText">Michael Coniaris</div>
+            <div className="HeaderImage">
                 <img 
                     alt="Michael Conairis's Website." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
             </div>
-            <div className="site-link-container">
+            <div className="SiteLinkContainer Full RowTwo">
                 <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
                 <div className="site-link-divider"> | </div>
                 <div className="site-link"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
