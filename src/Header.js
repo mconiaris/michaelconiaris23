92a26693cb3d4b12225318de1abe45f1575f32ca
mconiaris <mconiaris@gmail.com>
2023-05-31 16:56:50 -0400

    Changed flow of Banner to inline.

--- src/Header.js
+++ src/Header.js
@@ -4,20 +4,18 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="AppHeader top full">
-            <div className="HeaderImage">
+            <div className="Banner">
                 <img 
                     alt="Michael Conairis's Website." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
-            </div>
-            <div className="SiteTitleText">Michael Coniaris</div>
-            <div className="HeaderImage">
+                <div className="SiteTitleText">Michael Coniaris</div>
                 <img 
                     alt="Michael Conairis's Website." 
                     src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
                 </img>
-            </div>
-            <div className="SiteLinkContainer Full RowTwo">
+            </div>   
+            <div className="SiteLinkContainer full row-two">
                 <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
                 <div className="site-link-divider"> | </div>
                 <div className="site-link"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
