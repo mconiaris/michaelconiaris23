f0bce05bd567853809fd3058116be9f65babae32
mconiaris <mconiaris@gmail.com>
2023-05-31 17:22:37 -0400

    Defined Banner with flex display, wrapped banner images in a div and aligned items to center.

--- src/Header.js
+++ src/Header.js
@@ -5,15 +5,20 @@ export default function Header() {
     return (
         <header className="AppHeader top full">
             <div className="Banner">
-                <img 
-                    alt="Michael Conairis's Website." 
-                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
-                </img>
+                <div className="BannerImg">
+                    <img 
+                        alt="Michael Conairis's Website." 
+                        src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                    </img>
+                </div>
+                
                 <div className="SiteTitleText">Michael Coniaris</div>
-                <img 
-                    alt="Michael Conairis's Website." 
-                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
-                </img>
+                <div className="BannerImg">
+                    <img 
+                        alt="Michael Conairis's Website." 
+                        src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                    </img>
+                </div>
             </div>   
             <div className="SiteLinkContainer full row-two">
                 <div className="site-link"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
