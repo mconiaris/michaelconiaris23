c891890258ca548437d4748f26541088318fee67
mconiaris <mconiaris@gmail.com>
2023-06-08 14:20:21 -0400

    Reformatted header CSS. Changed structure using grid just for header.

--- src/Header.js
+++ src/Header.js
@@ -4,20 +4,24 @@ import './Header.css';
 export default function Header() {
     return (
         <header className="AppHeader">
-            <div className="Banner">
-                <div className="BannerImg">
-                    <img 
-                        alt="Michael Conairis's Website." 
-                        src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
-                    </img>
-                </div>
-                
-                <div className="SiteTitleText">Michael Coniaris</div>
-            </div>   
-            <div className="SiteLinkContainer">
+            <div className="BannerImg double left">
+                <img 
+                    alt="Michael Conairis's Website." 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                </img>
+            </div>
+            
+            <div className="SiteTitleText middle">Michael Coniaris</div>
+            <div className="SiteLinkContainer middle">
                 <div className="SiteLink"><a href="https://github.com/mconiaris">GitHub Profile</a></div>
                 <div className="SiteLinkDivider"> | </div>
                 <div className="SiteLink"><a href="https://www.linkedin.com/in/michaelconiaris/">LinkedIn Profile</a></div>
+            </div>
+            <div className="BannerImg double right">
+                <img 
+                    alt="Michael Conairis's Website." 
+                    src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/code_logo.jpg">
+                </img>
             </div>
         </header>
     )
