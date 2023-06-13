10b6ec3caee09a39c211b285a118fe605e0b3a6e
mconiaris <mconiaris@gmail.com>
2023-06-13 14:52:01 -0400

    Added hyperlinks to samples.

--- src/Main.js
+++ src/Main.js
@@ -6,23 +6,23 @@ export default function Main() {
         <div className="MainContainer">
             <div className="SamplesContainer">
                 <div className="Sample">
-                    <h2>spwgcards</h2>
+                    <h2><a href="http://www.spwgcards.com/">spwgcards</a></h2>
                     <div className="SampleText">
-                        <img 
+                        <a href="http://www.spwgcards.com/"><img 
                             alt="SPWG Card Maker Link." 
                             src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/SPWG+Card+maker+Screen+Shot.png">
-                        </img>
+                        </img></a>
                         <p>This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards.</p>
                         
                     </div>
                 </div>
                 <div className="Sample">
-                    <h2>Courtney Follweiler's Dance Therapy</h2>
+                    <h2><a href="http://www.courtneyfollweiler.com/">Courtney Follweiler's Dance Therapy</a></h2>
                     <div className="SampleText">
-                        <img 
+                        <a href="http://www.courtneyfollweiler.com/"><img 
                             alt="Courtney Follweiler's Website." 
                             src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/Courtney-Follweiler.png">
-                        </img>
+                        </img></a>
                         <p>This is sample text for Courtney's Website.</p>
                     </div>
                 </div>
