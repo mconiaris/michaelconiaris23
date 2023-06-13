5d7847323150819bedf11ceb5a77d51454206d36
mconiaris <mconiaris@gmail.com>
2023-06-13 15:25:19 -0400

    Added text for each sample.

--- src/Main.js
+++ src/Main.js
@@ -12,7 +12,7 @@ export default function Main() {
                             alt="SPWG Card Maker Link." 
                             src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/SPWG+Card+maker+Screen+Shot.png">
                         </img></a>
-                        <p>This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards. This is sample text for spwgcards.</p>
+                        <p>Developed using Ruby on Rails (<a href="https://github.com/mconiaris/spwg_card_maker">GitHub</a>), this app creates playable cards for the <a href="https://prowrestlingsuperstar.com/">Pro Wrestling Superstar</a> game. As the user enters in moves, points and other values to create a card, the app provides detailed statistics on its strength. The site also allows users to use existing templates to create their own playable cards.</p>
                         
                     </div>
                 </div>
@@ -23,7 +23,7 @@ export default function Main() {
                             alt="Courtney Follweiler's Website." 
                             src="https://s3.us-east-2.amazonaws.com/media.michaelconiaris.com/images/Courtney-Follweiler.png">
                         </img></a>
-                        <p>This is sample text for Courtney's Website.</p>
+                        <p>Developed using Ruby on Rails (<a href="https://github.com/mconiaris/courtney">GitHub</a>), this site serves as a marketing platform for a Dance Therapy company. The client wanted a straightforard and simple design.</p>
                     </div>
                 </div>
             </div>
